c52df7b289e5abd333cd709c16d4064e09cf81aa
manuelgloeckler <manu.gloeckler@hotmail.de>
2023-12-11 10:28:59 +0100

    Min graph works great

--- scoresbibm/main/tasks.py
+++ scoresbibm/main/tasks.py
@@ -22,6 +22,11 @@ class SBIBMTask:
             return _get_torch_task(self.name).get_simulator()
         else:
             raise NotImplementedError()
+    
+    def get_graph_mask(self):
+
+        raise NotImplementedError()
+
 
     def get_thetas_xs(self, num_samples: int):
         try:
@@ -84,21 +89,65 @@ class VariableConditionalTask(SBIBMTask):
 class LinearGaussian(SBIBMTask):
     def __init__(self, backend: str = "torch") -> None:
         super().__init__(name="gaussian_linear", backend=backend)
+        
+    def get_graph_mask(self):
+        task = _get_torch_task(self.name)
+        theta_dim = task.dim_parameters
+        x_dim = task.dim_data
+        thetas_mask = jnp.eye(theta_dim, dtype=jnp.bool_)
+        x_i_mask = jnp.eye(x_dim, dtype=jnp.bool_)
+        base_mask = jnp.block([[thetas_mask, jnp.zeros((theta_dim, x_dim))], [jnp.eye((x_dim)), x_i_mask]])
+        return base_mask.astype(jnp.bool_)
+
 
 
 class MixtureGaussian(SBIBMTask):
     def __init__(self, backend: str = "torch") -> None:
         super().__init__(name="gaussian_mixture", backend=backend)
+        
+    def get_graph_mask(self):
+        task = _get_torch_task(self.name)
+        theta_dim = task.dim_parameters
+        x_dim = task.dim_data
+        thetas_mask = jnp.eye(theta_dim, dtype=jnp.bool_)
+        x_mask = jnp.tril(jnp.ones((theta_dim, x_dim), dtype=jnp.bool_))
+        base_mask = jnp.block([[thetas_mask, jnp.zeros((theta_dim, x_dim))], [jnp.ones((x_dim, theta_dim)), x_mask]])
+
+        return base_mask.astype(jnp.bool_)
+        
+    
 
 
 class TwoMoons(SBIBMTask):
     def __init__(self, backend: str = "torch") -> None:
         super().__init__(name="two_moons", backend=backend)
+        
+    def get_graph_mask(self):
+        task = _get_torch_task(self.name)
+        theta_dim = task.dim_parameters
+        x_dim = task.dim_data
+        thetas_mask = jnp.eye(theta_dim, dtype=jnp.bool_)
+        x_mask = jnp.tril(jnp.ones((theta_dim, x_dim), dtype=jnp.bool_))
+        base_mask = jnp.block([[thetas_mask, jnp.zeros((theta_dim, x_dim))], [jnp.ones((x_dim, theta_dim)), x_mask]])
 
+        return base_mask.astype(jnp.bool_)
+        
 
 class SLCP(SBIBMTask):
     def __init__(self, backend: str = "torch") -> None:
         super().__init__(name="slcp", backend=backend)
+        
+    def get_graph_mask(self):
+        task = _get_torch_task(self.name)
+        theta_dim = task.dim_parameters
+        x_dim = task.dim_data
+        thetas_mask = jnp.eye(theta_dim, dtype=jnp.bool_) 
+        # TODO This could be triangular -> DAG
+        x_i_dim = x_dim // 4
+        x_i_mask = jax.scipy.linalg.block_diag(*tuple([jnp.tril(jnp.ones((x_i_dim,x_i_dim), dtype=jnp.bool_))]*4)) 
+        base_mask = jnp.block([[thetas_mask, jnp.zeros((theta_dim,x_dim))], [jnp.ones((x_dim, theta_dim)), x_i_mask]]) 
+        return base_mask.astype(jnp.bool_)
+
 
 
 def get_task(name: str, backend: str = "torch"):
