64bb0eb31ae16bfb285a880ee2a92d08555f8bef
manuelgloeckler <manu.gloeckler@hotmail.de>
2023-12-21 19:58:47 +0100

    MCMC, metrics....

--- probjax/nn/tokenizer.py
+++ probjax/nn/tokenizer.py
@@ -48,7 +48,7 @@ class Tokenizer(hk.Module):
         learn_meta_data_embeding: bool = False,
         name: str | None = "tokenizer",
     ):
-        """ Base class for tokenizers."""
+        """Base class for tokenizers."""
         self.output_dim = output_dim
         self.value_embeding_builder = value_embeding_builder
         self.node_embeding_builder = node_embeding_builder
@@ -119,7 +119,7 @@ class ScalarTokenizer(Tokenizer):
 
         data_id_embeding = self.node_embeding(data_id, output_dim1)
         data_embeding = self.value_embeding(data, output_dim2)
-        
+
         if meta_data is not None:
             meta_data_embeding = self.meta_data_embeding(meta_data, output_dim3)
         else:
@@ -200,8 +200,10 @@ class ScalarTokenizer(Tokenizer):
     @hk.transparent
     def meta_data_embeding(self, meta_data, output_dim):
         if self.meta_data_embeding_builder is None:
-            meta_data_embeding_fn = hk.Sequential([GaussianFourierEmbedding(128), hk.Linear(output_dim)])
-            #meta_data_embeding_fn = lambda x: jnp.repeat(x, output_dim, axis=-1)
+            meta_data_embeding_fn = hk.Sequential(
+                [GaussianFourierEmbedding(128), hk.Linear(output_dim)]
+            )
+            # meta_data_embeding_fn = lambda x: jnp.repeat(x, output_dim, axis=-1)
         else:
             meta_data_embeding_fn = self.meta_data_embeding_builder(self.output_dim)
 
