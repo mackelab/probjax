1463d17d9a5f4d5d79b030e829a73dd051c000e7
manuelgloeckler <manu.gloeckler@hotmail.de>
2024-06-21 08:33:32 +0200

    Gibbs and so one

--- probjax/inference/filter.py
+++ probjax/inference/filter.py
@@ -1,5 +1,3 @@
-
-
 from typing import Optional, Sequence, Callable
 import jax 
 import jax.numpy as jnp
@@ -11,37 +9,35 @@ from probjax.inference.filtering.base import FilterState, FilterInfo, FilterKern
 from probjax.inference.filtering.particle_filter import ParticleFilter
 
 
-
-
-
 def filter(key: PRNGKey, ts: ArrayLike, t_o: Optional[ArrayLike], x_o: Optional[ArrayLike], kernel: FilterKernel, *args, unpack_fn : Optional[Callable]=None, checkpoint_lengths: Optional[Sequence[int]]=None, unroll:int=1, **kwargs):
 
     state = kernel.init(*args, **kwargs)
     if unpack_fn is None:
         unpack_fn = get_default_unpack_fn(kernel)
-    
+
     def scan_fn(carry, t):
         state, key, i = carry
         key, subkey = jax.random.split(key)
         is_observed = t == t_o[i]
         def update_fn(subkey, state, i):
-            state, info = kernel(subkey,state, t_o[i], observed=x_o[i])
+            state, info = kernel(state, t_o[i], observed=x_o[i], rng_key=subkey)
             return state,info, i+1
-        def predict_fn(subkey, state,i):
-            state, info = kernel(subkey,state, t_o[i])
+
+        def predict_fn(subkey, state, i):
+            state, info = kernel(state, t_o[i], rng_key=subkey)
             return state, info, i
-        
+
         state, info, i = jax.lax.cond(is_observed, update_fn, predict_fn,subkey, state, i)
         out = unpack_fn(state, info)
         return (state, key, i), out
-    
+
     carry = (state, key, 0)
-    
+
     if checkpoint_lengths is None:
         _, output = jax.lax.scan(scan_fn, carry, ts, unroll=unroll)
     else:
         _, output = nested_checkpoint_scan(scan_fn, carry, ts, nested_lengths=checkpoint_lengths, unroll=unroll)
-   
+
     return output
 
 
@@ -51,13 +47,12 @@ def filter_log_likelihood(key, ts, t_o, x_o, kernel, *args, checkpoint_lengths=N
     return ll
 
 
-
 def get_default_unpack_fn(kernel: FilterKernel):
 
     if isinstance(kernel, ParticleFilter):
         return lambda state, info: state.particles
     else:
         return lambda state, info: (state, info)
-    
+
 def unpack_loglikeliood(state: FilterState, info: FilterInfo):
     return info.logZ
