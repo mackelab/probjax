74ed256f6c6b8a9ab13e26b22ffe0781f373048d
manuelgloeckler <manu.gloeckler@hotmail.de>
2024-06-21 08:33:44 +0200

    gibbs fix

--- probjax/inference/kernels/__init__.py
+++ probjax/inference/kernels/__init__.py
@@ -1,5 +1,4 @@
-
-from probjax.inference.kernels.gibbs import GibbsKernel
+from probjax.inference.kernels.gibbs import gibbs
 from probjax.inference.kernels.independent_mh import GaussianIMHKernel
 from probjax.inference.kernels.metropolis_hasting import GaussianMHKernel
 from probjax.inference.kernels.hmc import HMCKernel, NUTSKernel
