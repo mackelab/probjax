609cbe353d12edd8929f787242af81fc585d6b06
manuelgloeckler <manug@Manuels-MacBook-Pro.local>
2026-03-02 10:52:27 +0100

    fix

--- probjax/stats/bijective/custom_inverses.py
+++ probjax/stats/bijective/custom_inverses.py
@@ -8,7 +8,6 @@ from jax.typing import ArrayLike
 from probjax.core import custom_inverse
 
 
-@partial(custom_inverse, inv_argnum=1)
 def affine_bijector(
     params: ArrayLike,
     x: ArrayLike,
@@ -23,26 +22,6 @@ def affine_bijector(
     return loc + scale * x
 
 
-def _inv_and_logdet_affine_bijector(
-    params: ArrayLike,
-    y: ArrayLike,
-    min_scale: float = 1e-3,
-    **kwargs,
-):
-    del kwargs
-    y = jnp.asarray(y)
-    loc, scale = jnp.split(params, 2, axis=-1)
-    loc, scale = loc.reshape(y.shape), scale.reshape(y.shape)
-    scale = jnp.exp(scale) + min_scale
-    x = (y - loc) / scale
-    logdet = -jnp.sum(jnp.log(jnp.abs(scale)), axis=-1)
-    return x, logdet
-
-
-affine_bijector.definv_and_logdet(_inv_and_logdet_affine_bijector)
-
-
-@partial(custom_inverse, inv_argnum=1)
 def additive_bijector(
     params: ArrayLike,
     x: ArrayLike,
@@ -53,22 +32,6 @@ def additive_bijector(
     params = jnp.asarray(params)
     params = params.reshape(x.shape)
     return x + params
-
-
-def _inv_and_logdet_additive_bijector(
-    params: ArrayLike,
-    y: ArrayLike,
-    **kwargs,
-):
-    del kwargs
-    y = jnp.asarray(y)
-    params = jnp.asarray(params).reshape(y.shape)
-    x = y - params
-    logdet = jnp.zeros(y.shape[:-1], dtype=y.dtype)
-    return x, logdet
-
-
-additive_bijector.definv_and_logdet(_inv_and_logdet_additive_bijector)
 
 
 @partial(custom_inverse, inv_argnum=1)
