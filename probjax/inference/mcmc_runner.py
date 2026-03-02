f49a7282b504af62324839db7d1b2c2b196e795e
manuelgloeckler <manug@Manuels-MacBook-Pro.local>
2026-03-02 16:00:58 +0100

    rename

--- probjax/inference/mcmc_runner.py
+++ probjax/inference/mcmc_runner.py
@@ -53,7 +53,7 @@ class MCMC(WithProgressBarAPI):
             update_stats = lambda stats, _, y: tuple([
                 self._state_gamma * stats[i] + (1 - self._state_gamma) * y[i]
                 for i in range(len(stats))
- c           ])
+            ])
             print_fn = lambda i, total, state: self._print_progress(
                 type(self), i, total, state
             )
